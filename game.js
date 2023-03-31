2074d2069aaa91646b4c6572b32001f73bd072f7
Kim <ktla3223@gmail.com>
2023-03-31 03:46:32 -0700

    Added lines 1-8 functions

---
+++ game.js
@@ -0,0 +1,8 @@
+function getComputerChoice ('Rock', 'Paper', 'Scissors');
+function playRound(playSelection, computerSelection) {
+
+};
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
