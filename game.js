c57b9e61301d6930be0602985f74ad2b1b0aa715
Kim <ktla3223@gmail.com>
2023-04-05 12:50:19 -0700

    Added function rps on lines 23-42

--- game.js
+++ game.js
@@ -10,12 +10,35 @@ gameStart();
 scoreUpdate();
 
 function computerPlay () {
-    computerScore === Math.floor(Math.random() * (3) + 1);
+    computerSelection === Math.floor(Math.random() * (3) + 1);
     if (computerScore === 1) {
         results.textContent = "rock"; 
     } else if (computerScore === 2) {
         results.textContent = "paper"; 
     } else {
         results.textContent = "scissors";
-    }
+    }  
 } 
+
+function rps (playerSelection, computerSelection) {
+    playerSelection === playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+        ++playerScore;
+        scoreUpdate();
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+        ++playerScore;
+        scoreUpdate();
+    } else if (playerSelection === "scissors" && computerScore === "paper") {
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+        ++playerScore;
+        scoreUpdate();
+    } else {
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
+        ++computerScore;
+        scoreUpdate();
+    }
+}
+
+
