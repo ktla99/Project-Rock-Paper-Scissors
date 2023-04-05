bae679fa8f541767ee6ac08c9c41b74a5679d212
Kim <ktla3223@gmail.com>
2023-04-05 12:44:05 -0700

    Added function computerPlay to lines 12-21

--- game.js
+++ game.js
@@ -6,3 +6,16 @@ const score = document.querySelector('.score');
 const rps = document.querySelectorAll('#rps');
 const restart = document.querySelector('.restart');
 
+gameStart();
+scoreUpdate();
+
+function computerPlay () {
+    computerScore === Math.floor(Math.random() * (3) + 1);
+    if (computerScore === 1) {
+        results.textContent = "rock"; 
+    } else if (computerScore === 2) {
+        results.textContent = "paper"; 
+    } else {
+        results.textContent = "scissors";
+    }
+} 
